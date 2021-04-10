5a666f229cac10ebb13e2288ecff27173b068069
Nick Garner <nrgarner@ncsu.edu>
2021-04-10 10:17:42 -0400

    Changed mem to 4GB, added proc kill and gates to itrust.yml

--- commands/setup.js
+++ commands/setup.js
@@ -23,51 +23,43 @@ exports.builder = (yargs) => {
     type: "string",
     description: "NCSU GitHub Password",
   });
-  yargs.demandOption(
-    ["gh-user", "gh-pass"],
-    "Please provide NCSU Github credentials.",
-  );
+  yargs.demandOption(["gh-user", "gh-pass"], "Please provide NCSU Github credentials.");
 };
 
 exports.handler = async (argv) => {
   const { privateKey, "gh-user": ghUser, "gh-pass": ghPass } = argv;
 
   (async () => {
-    await run({privateKey, ghUser, ghPass});
+    await run({ privateKey, ghUser, ghPass });
   })();
 };
 
-async function run({privateKey, ghUser, ghPass}) {
+async function run({ privateKey, ghUser, ghPass }) {
   console.log(chalk.greenBright("Installing config-srv server!"));
 
   console.log(chalk.blueBright("Downloading focal image..."));
-  let result = child.spawnSync(
-    `bakerx`,
-    `pull focal cloud-images.ubuntu.com`.split(" "),
-    {
-      shell: true,
-      stdio: "inherit",
-    }
-  );
+  let result = child.spawnSync(`bakerx`, `pull focal cloud-images.ubuntu.com`.split(" "), {
+    shell: true,
+    stdio: "inherit",
+  });
   if (result.error) {
     printError(result);
   }
 
   console.log(chalk.blueBright("Provisioning config-srv server..."));
-  result = child.spawnSync(
-    `bakerx`,
-    `run config-srv focal --ip 192.168.33.20 --memory=2048 --sync`.split(" "),
-    {
-      shell: true,
-      stdio: "inherit",
-    }
-  );
+  result = child.spawnSync(`bakerx`, `run config-srv focal --ip 192.168.33.20 --memory=4096 --sync`.split(" "), {
+    shell: true,
+    stdio: "inherit",
+  });
   if (result.error) {
     printError(result);
   }
 
   console.log(chalk.blueBright("Running init script..."));
-  result = sshSync(`/bakerx/cm/server-init.sh ${ghUser ? `-u ${ghUser}` : ''} ${ghPass ? `-p ${ghPass}` : ''}`, "vagrant@192.168.33.20");
+  result = sshSync(
+    `/bakerx/cm/server-init.sh ${ghUser ? `-u ${ghUser}` : ""} ${ghPass ? `-p ${ghPass}` : ""}`,
+    "vagrant@192.168.33.20"
+  );
   if (result.error) {
     printError(result);
   }
